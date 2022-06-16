fb204d035b917eceb447f033a9c9bef487776ad7
jscarlson <jcarlson238@gmail.com>
2022-06-16 15:48:57 -0400

    dev

--- eval.py
+++ eval.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
         worddicts_r[vv] = kk
 
     # run OCR
-    for image_path in glob(os.path.join(TEST_IMAGES, "*")):
+    for image_path in glob(os.path.join(TEST_IMAGES, "*.png")):
         print(image_path)
         result = Kindai_OCR(image_path)
         print(result)
